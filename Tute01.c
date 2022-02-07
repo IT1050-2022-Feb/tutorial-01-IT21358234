81ccd8670e0b04cab6d5ae08cca403d2ffb37136
IT21358234 <99161169+IT21358234@users.noreply.github.com>
2022-02-07 06:22:04 +0000

    What did you change

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1, mark2;
+  float avg;
+
+  printf("Enter mark for subject 1: ");
+  scanf("%d",&mark1);
+  printf("Enter mark for subject 2: ");
+  scanf("%d",&mark2);
+
+  avg=(mark1+mark2)/2.0;
+
+  printf("average is %.2f",avg);
   
   return 0;
 }
--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,18 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+
+  int n, i, sum=0;
+	
+	printf("input a number as n: ");
+	scanf ("%d",&n);
+	
+	for(i=1;i<=n;++i)
+	{
+		sum=sum+i;
+	}
+	
+	printf("sum of the numbers from 1 to 'n' = %d",sum);
   
   return 0;
 }
